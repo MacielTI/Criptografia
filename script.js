76f3d5a50fb975b5539cae7f8909d42d062c3d79
Bruno do Nascimento Maciel <brunonascimentomaciel@hotmail.com>
2016-01-25 23:17:42 -0200

    simplificando codigo e interface grafica

---
+++ script.js
@@ -0,0 +1,235 @@
+$(document).ready(function(){
+  $(".encriptar").click(function(){
+    var prim=0;
+    var end = 1;
+    var caracternumber =0;
+    var insertcode =0;
+    var char=["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+    var text = prompt("Para Encriptar");
+    for (var i = 0; i < 100 ; i++) {
+      if(text.substring(prim,end) ==="a")
+      {
+        char[insertcode] = "10210548"
+      }
+      if(text.substring(prim,end) ==="A")
+      {
+        char[insertcode] = "87875487"
+      }
+      if(text.substring(prim,end) === "b")
+      {
+        char[insertcode] = "78954210"
+      }
+      if(text.substring(prim,end) ==="B")
+      {
+        char[insertcode] = "63276568"
+      }
+      if(text.substring(prim,end)==="c")
+      {
+        char[insertcode] = "45651215"
+      }
+      if(text.substring(prim,end)==="C")
+      {
+        char[insertcode] = "65465476"
+      }
+      if(text.substring(prim,end)==="d")
+      {
+        char[insertcode]="53491965"
+      }
+      if(text.substring(prim,end)==="D")
+      {
+        char[insertcode] = "78471232"
+      }
+      if(text.substring(prim,end)==="e")
+      {
+        char[insertcode] = "98794565"
+      }
+      if(text.substring(prim,end)==="E")
+      {
+        char[insertcode] = "87986456"
+      }
+      if(text.substring(prim,end)==="f")
+      {
+        char[insertcode] = "98415362"
+      }
+      if(text.substring(prim,end)==="F")
+      {
+        char[insertcode] = "87984515"
+      }
+      if(text.substring(prim,end)==="g")
+      {
+        char[insertcode] = "32164699"
+      }
+      if(text.substring(prim,end)==="G")
+      {
+        char[insertcode] = "45451316"
+      }
+      if(text.substring(prim,end)==="h")
+      {
+        char[insertcode] = "43658297"
+      }
+      if(text.substring(prim,end)==="H")
+      {
+        char[insertcode] = "35842196"
+      }
+      if(text.substring(prim,end)==="i")
+      {
+        char[insertcode] = "16485935"
+      }
+      if(text.substring(prim,end)==="I")
+      {
+        char[insertcode] = "25649318"
+      }
+      if(text.substring(prim,end)==="j")
+      {
+        char[insertcode] = "97510823"
+      }
+      if(text.substring(prim,end)==="J")
+      {
+        char[insertcode] = "86304572"
+      }
+      if(text.substring(prim,end)==="k")
+      {
+        char[insertcode] = "02369458"
+      }
+      if(text.substring(prim,end)==="K")
+      {
+        char[insertcode] = "82041679"
+      }
+      if(text.substring(prim,end)==="l")
+      {
+        char[insertcode] = "53609178"
+      }
+      if(text.substring(prim,end)==="L")
+      {
+        char[insertcode] = "89472315"
+      }
+      if(text.substring(prim,end)==="m")
+      {
+        char[insertcode] = "03876541"
+      }
+      if(text.substring(prim,end)==="M")
+      {
+        char[insertcode] = "25091786"
+      }
+      if(text.substring(prim,end)==="n")
+      {
+        char[insertcode] = "57043162"
+      }
+      if(text.substring(prim,end)==="N")
+      {
+        char[insertcode] ="62513847"
+      }
+      if(text.substring(prim,end)==="o")
+      {
+        char[insertcode] = "91508436"
+      }
+      if(text.substring(prim,end)==="O")
+      {
+        char[insertcode] = "76340895"
+      }
+      if(text.substring(prim,end)==="p")
+      {
+        char[insertcode] = "84652370"
+      }
+      if(text.substring(prim,end)==="P")
+      {
+        char[insertcode] = "98021675"
+      }
+      if(text.substring(prim,end)==="q")
+      {
+        char[insertcode] = "65189074"
+      }
+      if(text.substring(prim,end)==="Q")
+      {
+        char[insertcode] = "86045291"
+      }
+      if(text.substring(prim,end)==="r")
+      {
+        char[insertcode] = "65948173"
+      }
+      if(text.substring(prim,end)==="R")
+      {
+        char[insertcode] = "05163879"
+      }
+      if(text.substring(prim,end)==="s")
+      {
+        char[insertcode] = "47502816"
+      }
+      if(text.substring(prim,end)==="S")
+      {
+        char[insertcode] = "19564327"
+      }
+      if(text.substring(prim,end)==="t")
+      {
+        char[insertcode] = "21647853"
+      }
+      if(text.substring(prim,end)==="T")
+      {
+        char[insertcode] = "79681304"
+      }
+      if(text.substring(prim,end)==="u")
+      {
+        char[insertcode] = "78341956"
+      }
+      if(text.substring(prim,end)==="U")
+      {
+        char[insertcode] = "26349850"
+      }
+      if(text.substring(prim,end)==="v")
+      {
+        char[insertcode] = "54392176"
+      }
+      if(text.substring(prim,end)==="V")
+      {
+        char[insertcode] = "16295738"
+      }
+      if(text.substring(prim,end)==="w")
+      {
+        char[insertcode] = "14370285"
+      }
+      if(text.substring(prim,end)==="W")
+      {
+        char[insertcode] = "19483520"
+      }
+      if(text.substring(prim,end)==="x")
+      {
+        char[insertcode] = "38561942"
+      }
+      if(text.substring(prim,end)==="X")
+      {
+        char[insertcode] = "23619785"
+      }
+      if(text.substring(prim,end)==="y")
+      {
+        char[insertcode] = "39710485"
+      }
+      if(text.substring(prim,end)==="Y")
+      {
+        char[insertcode] = "71690402"
+      }
+      if(text.substring(prim,end)==="z")
+      {
+        char[insertcode] = "94573861"
+      }
+      if(text.substring(prim,end)==="Z")
+      {
+        char[insertcode] = "40822685"
+      }
+      if(text.substring(prim,end)===" ")
+      {
+        char[insertcode] = "65465166"
+      }
+      //charracters
+      if (text.substring(prim,end)==="á") {
+        char[insertcode] ="18742539"
+      }
+      if (text.substring(prim,end)==="é") {
+        char[insertcode] ="50427936"
+      }
+    prim = prim+1;
+    end = end+1;
+    insertcode= insertcode+1;
+    }
+    alert(char[0]+""+char[1]+""+char[2]+""+char[3]+""+char[4]+""+char[5]+""+char[6]+""+char[7]+""+char[8]+""+char[9]+""+char[10]+""+char[11]+""+char[12]+""+char[13]+""+char[14]+""+char[15]+""+char[16]+""+char[17]+""+char[18]+""+char[19]+""+char[20]+""+char[21]+""+char[22]+""+char[23]);
+  });
+});
